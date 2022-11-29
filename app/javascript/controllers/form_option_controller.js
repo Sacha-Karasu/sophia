9630685c95318722dcff5e08278a5ef33397c411
Sacha Karasu <marie.karasu@gmail.com>
2022-11-29 18:25:12 +0100

    toggle and form for new room ok

--- app/javascript/controllers/form_option_controller.js
+++ app/javascript/controllers/form_option_controller.js
@@ -2,13 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="form-option"
 export default class extends Controller {
+  static targets = ["noend", "end"]
+
   connect() {
 
   }
 
   displayForm() {
-    this.noendTarget.classList.add("d-none")
-    this.noendTarget.classList.add("d-none")
+    this.noendTarget.classList.toggle("d-none")
+    this.endTarget.classList.toggle("d-none")
   }
 
 
