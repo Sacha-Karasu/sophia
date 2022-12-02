2028a61ffed57ce3750663b7a3d3ec511669b3a3
Claire Deville <clairedeville@gmail.com>
2022-12-02 13:26:00 +0100

    Add a list of user rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -36,6 +36,8 @@ class RoomsController < ApplicationController
         info_window: render_to_string(partial: "info_window", locals: {room: room})
       }
     end
+
+    @rooms_with_messages_in = current_user.rooms
   end
 
   def destroy
