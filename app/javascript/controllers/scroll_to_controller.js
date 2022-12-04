6aff4dcdf0c721fe6e7cb03ce0767ebe740e80c3
Julia Weiss <juliaweiss@live.fr>
2022-12-04 22:13:15 +0100

    scroll behavior for favs rooms

--- app/javascript/controllers/scroll_to_controller.js
+++
@@ -1,8 +0,0 @@
-// import ScrollTo from 'stimulus-scroll-to'
-
-// export default class extends ScrollTo {
-//   connect() {
-//     super.connect()
-//     console.log('Do what you want here.')
-//   }
-// }
