365eec334fc560248f6a044711e57922b15b4b97
Julia Weiss <juliaweiss@live.fr>
2022-12-06 12:40:34 +0100

    correct user location controller protocol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :verify_authenticity_token, only: :location
 
   def location
